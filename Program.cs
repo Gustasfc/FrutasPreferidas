60ad2978a0f7d00526547e95bfae88e73da473e5
gustasfc <gustavorgonzalezgja@gmail.com>
2025-02-24 11:05:07 -0300

    Finaliza o Programa das 5 frutas favoritas

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.Clear();
+Console.WriteLine("########################");
+Console.WriteLine("### Frutas favoritas ###");
+Console.WriteLine("########################");
+
+Console.WriteLine();
+
+Console.WriteLine("1. ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Banana");
+
+Console.WriteLine("2. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Maça");
+
+Console.WriteLine("3. ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Maracuja");
+
+
+Console.WriteLine("4. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Morango");
+
+Console.WriteLine("5. ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Abacate");
+Console.ResetColor();
